0f034babe4dfc8b201580ab2b833a9033fa2ca81
Adam Pahlevi <adam.pahlevi@gmail.com>
2017-03-28 14:20:15 +0700

    redis can read from REDIS_URL

--- main.go
+++ main.go
@@ -4,16 +4,27 @@ import (
 	"flag"
 	"fmt"
 	"github.com/go-redis/redis"
+	"os"
 	"strconv"
 )
 
 var (
-	Redis = redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:6379",
+	Redis = initRedis()
+)
+
+func initRedis() *redis.Client {
+	redisUrl := os.Getenv("REDIS_URL")
+
+	if redisUrl == "" {
+		redisUrl = "127.0.0.1:6379"
+	}
+
+	return redis.NewClient(&redis.Options{
+		Addr:     redisUrl,
 		Password: "",
 		DB:       0,
 	})
-)
+}
 
 func main() {
 	act := flag.String("act", "producer", "Either: producer or consumer")
